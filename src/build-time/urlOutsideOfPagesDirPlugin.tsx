3154cc4b94d72ae78f38b60f1bba415d06d0d987
Piotr Monwid-Olechnowicz <hasparus@gmail.com>
2022-10-03 18:21:21 +0800

    Move all markdown to posts directory

---
+++ src/build-time/urlOutsideOfPagesDirPlugin.tsx
@@ -0,0 +1,19 @@
+import type { RemarkPlugin } from "@astrojs/markdown-remark";
+import { relative } from "node:path";
+
+import type { PostProps } from "../types";
+
+export const urlOutsideOfPagesDirPlugin: RemarkPlugin<
+  [{ absoluteDirPath: string }]
+> = ({ absoluteDirPath }) => {
+  return (_tree, file) => {
+    const props = (file.data as { astro: PostProps }).astro;
+
+    // We can't assign to `props.url` so we take `frontmatter.path`
+    props.frontmatter.path =
+      "/" +
+      relative(absoluteDirPath, file.path)
+        .replace(/\\/g, "/")
+        .replace(file.extname || "", "");
+  };
+};
