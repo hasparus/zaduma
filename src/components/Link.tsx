5bc3a772da523953374b96631bc7b432bd75d6ad
Piotr Monwid-Olechnowicz <hasparus@gmail.com>
2022-10-03 18:43:49 +0800

    Support dark mode

--- src/components/Link.tsx
+++ src/components/Link.tsx
@@ -9,10 +9,13 @@ export function Link(props: JSX.AnchorHTMLAttributes<HTMLAnchorElement>) {
       classList={{
         ...own.classList,
         ["p-2 -mx-2" +
-        " rounded-sm underline decoration-gray-200 hover:decoration-transparent focus:decoration-transparent" +
+        " rounded-sm underline underline-offset-4" +
+        " decoration-gray-200 dark:decoration-gray-700" +
+        " hover:decoration-transparent dark:hover:decoration-transparent" +
+        " focus:decoration-transparent dark:focus:decoration-transparent" +
         " transition-colors relative" +
         " before:content-[''] before:absolute before:inset-0 before:-z-10 before:pointer-events-none" +
-        " hover:before:bg-gray-100 focus:before:bg-gray-100 before:transition-colors"]:
+        " hover:before:bg-gray-100 dark:hover:before:bg-gray-800/60 focus:before:bg-gray-100 dark:focus:bg-gray-800/60 before:transition-colors"]:
           true,
       }}
       {...rest}
