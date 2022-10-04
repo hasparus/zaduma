957f6dc510aabb1753fca473a85d1770ae58bd05
Piotr Monwid-Olechnowicz <hasparus@gmail.com>
2022-10-04 18:59:01 +0800

    Expose noUnderline prop in Link

--- src/lib/Link.tsx
+++ src/lib/Link.tsx
@@ -1,21 +1,26 @@
 import { JSX, splitProps } from "solid-js";
 
-export function Link(props: JSX.AnchorHTMLAttributes<HTMLAnchorElement>) {
-  const [own, rest] = splitProps(props, ["classList"]);
+export interface LinkProps extends JSX.AnchorHTMLAttributes<HTMLAnchorElement> {
+  noUnderline?: boolean;
+}
+
+export function Link(props: LinkProps) {
+  const [own, rest] = splitProps(props, ["classList", "noUnderline"]);
 
   return (
     // eslint-disable-next-line jsx-a11y/anchor-has-content
     <a
       classList={{
         ...own.classList,
-        ["p-2 -mx-2" +
-        " rounded-sm underline underline-offset-4" +
-        " decoration-gray-200 dark:decoration-gray-700" +
+        [" underline underline-offset-4 decoration-gray-200 dark:decoration-gray-700" +
         " hover:decoration-transparent dark:hover:decoration-transparent" +
-        " focus:decoration-transparent dark:focus:decoration-transparent" +
-        " transition-colors relative" +
+        " focus:decoration-transparent dark:focus:decoration-transparent"]:
+          !own.noUnderline,
+        ["p-2 -mx-2 whitespace-nowrap" +
+        " rounded-sm before:rounded-sm" +
+        " transition-colors relative before:transition-colors before:bg-transparent" +
         " before:content-[''] before:absolute before:inset-0 before:-z-10 before:pointer-events-none" +
-        " hover:before:bg-gray-100 dark:hover:before:bg-gray-800/60 focus:before:bg-gray-100 dark:focus:bg-gray-800/60 before:transition-colors"]:
+        " hover:before:bg-gray-100 dark:hover:before:bg-gray-800/60 focus:before:bg-gray-100 dark:focus:before:bg-gray-800/60 before:transition-colors"]:
           true,
       }}
       {...rest}
