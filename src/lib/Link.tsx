3920fff301efc9aa18174e6bda74fcb14bfba750
Piotr Monwid-Olechnowicz <hasparus@gmail.com>
2022-10-07 16:07:06 +0800

    Add controls.css and zm-hover-before

--- src/lib/Link.tsx
+++ src/lib/Link.tsx
@@ -16,12 +16,9 @@ export function Link(props: LinkProps) {
         " hover:decoration-transparent dark:hover:decoration-transparent" +
         " focus:decoration-transparent dark:focus:decoration-transparent"]:
           !own.noUnderline,
-        ["p-2 -mx-2 whitespace-nowrap" +
-        " rounded-sm before:rounded-sm" +
-        " transition-colors relative before:transition-colors before:bg-transparent" +
-        " before:content-[''] before:absolute before:inset-0 before:-z-10 before:pointer-events-none" +
-        " hover:before:bg-gray-100 dark:hover:before:bg-gray-800/60 focus:before:bg-gray-100 dark:focus:before:bg-gray-800/60 before:transition-colors"]:
+        "p-2 -mx-2 whitespace-nowrap rounded-sm before:rounded-sm transition-colors relative":
           true,
+        "zm-hover-before": true,
       }}
       {...rest}
     />
