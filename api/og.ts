a810d4f75fafae1a1c370f6e037ee23d4a246b9e
Piotr Monwid-Olechnowicz <hasparus@gmail.com>
2023-02-04 14:47:49 -0300

    Remove negative margin in OG image Title

--- api/og.ts
+++ api/og.ts
@@ -103,7 +103,7 @@ function Illustration({
     "div",
     {
       tw: `
-          flex flex-1 justify-start items-end w-full pt-4 px-4 -mb-1.5 relative
+          flex flex-1 justify-start items-end w-full pt-4 px-4 relative
           bg-[rgb(23,23,23)]
         `,
     },
