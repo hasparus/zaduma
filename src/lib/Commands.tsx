cbf06ba4ef4b608cf0bb3bb05a4632040f7fe738
beerose <alexsandra.sikora@gmail.com>
2023-02-16 15:04:52 +0100

    Make backspace close tabs only if input is empty

--- src/lib/Commands.tsx
+++ src/lib/Commands.tsx
@@ -20,6 +20,7 @@ import {
   CommandItem as CommandCenterItem,
   CommandItemProps as CommandCenterItemProps,
   CommandList,
+  useCommandCenterCtx,
 } from "./CommandCenter";
 import { DialogCloseButton } from "./Dialog";
 import { isMac } from "./isMac";
@@ -53,6 +54,8 @@ export function CommandsPalette({
   posts: { title: string; href: string }[];
 }) {
   type CommandsPage = "posts" | "theme" | undefined;
+  const { getInputValue } = useCommandCenterCtx();
+
   const [page, setPage] = createSignal<CommandsPage>();
   let dialog: HTMLDialogElement | undefined;
 
@@ -75,7 +78,13 @@ export function CommandsPalette({
     [
       "escape",
       () => {
-        // setPage(undefined);
+        setPage(undefined);
+      },
+    ],
+    [
+      "backspace",
+      () => {
+        if (!getInputValue()) setPage(undefined);
       },
     ],
     [
@@ -147,15 +156,7 @@ export function CommandsPalette({
         event.preventDefault();
         setPage(undefined);
       }}
-      ref={(ref) => {
-        dialog = ref;
-        dialog?.addEventListener("cancel", (event) => {
-          if (page() !== undefined) {
-            event.preventDefault();
-            setPage(undefined);
-          }
-        });
-      }}
+      ref={(ref) => (dialog = ref)}
       class={
         "backdrop:bg-white backdrop:bg-opacity-30" +
         " dark:backdrop:bg-black dark:backdrop:bg-opacity-30" +
