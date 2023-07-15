fa10306b26acbf5d2f0765fbdc693a26cc61d37d
Edson C <enerqy@gmail.com>
2023-07-16 00:22:09 +0200

    Deployment Woes #3
    Removed Vercel and added Astro.

--- astro.config.ts
+++ astro.config.ts
@@ -9,18 +9,16 @@ import { fileURLToPath } from "node:url";
 import { rehypePlugins, remarkPlugins } from "./src/build-time";
 
 const __filename = fileURLToPath(import.meta.url);
-
 const __dirname = dirname(__filename);
 
 // Production URL
-const hostname = "zaduma.vercel.app";
+const hostname = "incomparable-raindrop-37b18f.netlify.app"; // Change this to your actual Netlify app URL
 const site = `https://${hostname}/`;
 
 // https://astro.build/config
 export default defineConfig({
   site,
   markdown: {
-    // We'll highlight using Shiki Twoslash remark plugin
     syntaxHighlight: false,
   },
   integrations: [
@@ -51,23 +49,11 @@ export default defineConfig({
 });
 
 function makePublicURL() {
-  const VERCEL_URL = process.env.VERCEL_URL;
-  const DEPLOYMENT_ALIAS = process.env.DEPLOYMENT_ALIAS;
+  const NETLIFY_URL = process.env.URL;
 
-  // If the site is built on vercel, we can just use VERCEL_URL.
-  if (VERCEL_URL) return VERCEL_URL;
+  // If the site is built on Netlify, we can just use NETLIFY_URL.
+  if (NETLIFY_URL) return NETLIFY_URL;
 
-  if (!DEPLOYMENT_ALIAS) {
-    // If there's no DEPLOYMENT_ALIAS nor VERCEL_URL, we assume we're building locally.
-    return "http://localhost:3000/";
-  }
-
-  // Otherwise, we build on GitHub Actions (and get access to Git History).
-  // If DEPLOYMENT_ALIAS is set to `main--${hostname}`, we're on the main branch,
-  // and we return the canonical URL.
-  if (DEPLOYMENT_ALIAS === `main--${hostname}`) return site;
-
-  // Otherwise, we're building a preview deployment, and set the deployment alias
-  // in `import.meta.env.PUBLIC_URL`.
-  return `https://${DEPLOYMENT_ALIAS}`;
+  // If there's no NETLIFY_URL, we assume we're building locally.
+  return "http://localhost:3000/";
 }
