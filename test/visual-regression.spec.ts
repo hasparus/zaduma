7d4abc0e89dc4ca6396a9f0a03b246246e1f7a51
Piotr Monwid-Olechnowicz <hasparus@gmail.com>
2024-05-06 16:10:19 +0200

    Compare screenshots of blog posts

--- test/visual-regression.spec.ts
+++ test/visual-regression.spec.ts
@@ -1,10 +1,20 @@
 import { toMatchImageSnapshot } from "jest-image-snapshot";
+import { readdir } from "node:fs/promises";
+import { getDocument, queries } from "pptr-testing-library";
 import puppeteer, { type Browser, Page } from "puppeteer";
 import { afterAll, beforeAll, describe, expect, it } from "vitest";
 
 const HEADLESS = true;
 
 expect.extend({ toMatchImageSnapshot });
+
+let postsInFS = await readdir(new URL("../posts", import.meta.url).pathname, {
+  recursive: true,
+});
+postsInFS = postsInFS
+  .filter((file) => /\.mdx?$/.test(file))
+  .map((file) => file.replace(/\.mdx?$/, ""))
+  .sort();
 
 /**
  * We're doing visual testing in a blog starter to ensure
@@ -21,10 +31,45 @@ describe("visual regression", async () => {
     page = await browser.newPage();
   });
 
-  it("should match the snapshot", async () => {
+  let postsInFS = await readdir(new URL("../posts", import.meta.url).pathname, {
+    recursive: true,
+  });
+  postsInFS = postsInFS
+    .filter((file) => /\.mdx?$/.test(file))
+    .map((file) => file.replace(/\.mdx?$/, "").replaceAll(" ", "-"));
+
+  // the tests are ran in order with no file parallelism
+
+  it("renders links to all posts on index page", async () => {
     await page.goto("http://localhost:4321");
+
+    const document = await getDocument(page);
+    const handles = await queries.getAllByRole(document, "link");
+
+    const hrefs: string[] = [];
+    for (const handle of handles) {
+      const href = await handle.evaluate((el) => el.getAttribute("href"));
+      if (href) {
+        hrefs.push(href.replace(/^\//, ""));
+      }
+    }
+
+    for (const post of postsInFS) {
+      expect(hrefs).toContain(post);
+    }
+  });
+
+  it("matches screenshot on index page", async () => {
     const screenshot = await page.screenshot();
     expect(screenshot).toMatchImageSnapshot();
+  });
+
+  it("matches screenshot in blog posts", async () => {
+    for (const post of postsInFS) {
+      await page.goto(`http://localhost:4321/${post}`);
+      const screenshot = await page.screenshot();
+      expect(screenshot).toMatchImageSnapshot();
+    }
   });
 
   afterAll(async () => {
