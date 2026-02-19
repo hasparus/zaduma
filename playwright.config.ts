2803ff00cff45da369bf12a20b207b8757011bec
Piotr Monwid-Olechnowicz <hasparus@gmail.com>
2026-02-19 12:26:48 +0100

    Save test trace

--- playwright.config.ts
+++ playwright.config.ts
@@ -4,9 +4,9 @@ export default defineConfig({
   testDir: "./e2e",
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
-  retries: process.env.CI ? 2 : 0,
+  retries: process.env.CI ? 2 : 1,
   ...(process.env.CI ? { workers: 1 } : {}),
-  reporter: "html",
+  reporter: [["list", { printSteps: true }], ["html"]],
   snapshotPathTemplate: "{testDir}/{testFileDir}/{testFileName}-snapshots/{arg}-{projectName}{ext}",
   use: {
     baseURL: "http://localhost:4321",
